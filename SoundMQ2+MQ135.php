fff9d6e502a411b57c24638ee48f261d44635168
Alex Sokolov <Gelezako@users.noreply.github.com>
2017-07-28 12:08:22 +0300

    Update and rename SoundMQ2.php to SoundMQ2+MQ135.php

--- SoundMQ2+MQ135.php
+++ SoundMQ2+MQ135.php
@@ -1,5 +1,5 @@
 /*
-* MQ2.php
+* SoundMQ2+MQ135.php
 * @author Alex Sokolov <admin@gelezako.com>
 * @copyright Alex Sokolov http://www.blog.gelezako.com (c)
 * @version 0.1 ([May 23, 2017])
@@ -14,4 +14,13 @@ if($params['gas']=="воздуха"){
 if($params['gas']=="угарного газа"){
  	$gas="Уровень угарного газа ".gg("Kitchen.MQ2");
  	say("$gas",2);
+     if(gg("Kitchen.MQ135")<80) say("Это в пределах нормы.",2);
+     else if(gg("Kitchen.MQ135")>=80) say("Это за пределами нормы. Необходимо проветритьпомещение.",2);
+}
+
+if($params['gas']=="углекислый" or $params['gas']=="углекислого"){
+ 	$gas="Уровень углекислого газа ".gg("Kitchen.MQ135");
+ 	say("$gas",2);
+     if(gg("Kitchen.MQ135")<80) say("Это в пределах нормы.",2);
+     else if(gg("Kitchen.MQ135")>=80) say("Это за пределами нормы. Необходимо проветритьпомещение.",2);
 }
