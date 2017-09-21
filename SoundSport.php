fc3b4ab34031fd03dc862805b2223fbd855133c9
Alex Sokolov <Gelezako@users.noreply.github.com>
2017-09-21 16:45:00 +0300

    Create SoundSport.php

---
+++ SoundSport.php
@@ -0,0 +1,11 @@
+/**
+* SoundSport.php
+* @package project
+* @author Alex Sokolov <admin@gelezako.com>
+* @copyright http://blog.gelezako.com (c)
+* @version 0.1 ([Sep 21, 2017])
+*/
+
+if($params['exercises']=='пресс' or $params['exercises']=='прессу')sg("Press.Quantity",$params['quantity']);
+if($params['exercises']=='отжался' or $params['exercises']=='отжимание' or $params['exercises']=='отжиманию')sg("Pushup.Quantity",$params['quantity']);
+if($params['exercises']=='подтянулся' or $params['exercises']=='подтягиванию')sg("Pullup.Quantity",$params['quantity']);
