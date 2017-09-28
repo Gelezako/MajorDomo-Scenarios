91df7cdef5053e4b3b69104c25d27bf8d3d60642
Alex Sokolov <Gelezako@users.noreply.github.com>
2017-09-28 17:49:15 +0300

    Update SoundSport.php

--- SoundSport.php
+++ SoundSport.php
@@ -6,6 +6,6 @@
 * @version 0.1 ([Sep 21, 2017])
 */
 
-if($params['exercises']=='пресс' or $params['exercises']=='прессу')sg("Press.Quantity",$params['quantity']);
-if($params['exercises']=='отжался' or $params['exercises']=='отжимание' or $params['exercises']=='отжиманию')sg("Pushup.Quantity",$params['quantity']);
-if($params['exercises']=='подтянулся' or $params['exercises']=='подтягиванию')sg("Pullup.Quantity",$params['quantity']);
+if($params['exercises']=='пресс' or $params['exercises']=='прессу'){sg("Press.Quantity",$params['number']);say("Готово, записано ".$params['number'],2);}
+if($params['exercises']=='отжался' or $params['exercises']=='отжимание' or $params['exercises']=='отжиманию'){sg("Pushup.Quantity",$params['number']);say("Готово, записано ".$params['number'],2);}
+if($params['exercises']=='подтянулся' or $params['exercises']=='подтягиванию'){sg("Pullup.Quantity",$params['number']);say("Готово, записано ".$params['number'],2);}
