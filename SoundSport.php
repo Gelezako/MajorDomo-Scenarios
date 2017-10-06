e3e381d2aa6d31ae89063f535fb293cc0cbe4e34
Alex Sokolov <Gelezako@users.noreply.github.com>
2017-10-06 20:53:08 +0300

    Update SoundSport.php

--- SoundSport.php
+++ SoundSport.php
@@ -8,4 +8,4 @@
 
 if($params['exercises']=='пресс' or $params['exercises']=='прессу'){sg("Press.Quantity",$params['number']);say("Готово, записано ".$params['number'],2);}
 if($params['exercises']=='отжался' or $params['exercises']=='отжимание' or $params['exercises']=='отжиманию'){sg("Pushup.Quantity",$params['number']);say("Готово, записано ".$params['number'],2);}
-if($params['exercises']=='подтянулся' or $params['exercises']=='подтягиванию'){sg("Pullup.Quantity",$params['number']);say("Готово, записано ".$params['number'],2);}
+if($params['exercises']=='подтянулся' or $params['exercises']=='подтягиванию' or $params['exercises']=='подтягивание'){sg("Pullup.Quantity",$params['number']);say("Готово, записано ".$params['number'],2);}
