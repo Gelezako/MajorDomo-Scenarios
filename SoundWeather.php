4cd6db200bd26576d70266ea5a8cc3c488bf9b3a
Alex Sokolov <Gelezako@users.noreply.github.com>
2018-03-21 11:47:49 +0200

    исправления

--- SoundWeather.php
+++ SoundWeather.php
@@ -79,42 +79,36 @@ include_once(DIR_MODULES . 'app_openweather/app_openweather.class.php');
     } else {
      $stw=" Ветер капец какой сильный ";
     }
-	
+
+global $temp,$humidity,$wind,$weather_type;
 //определяем на когда спрашиваем погоду
-if((string)$params['weather']===(string)"погода" and $params['WeatherType']==""){
-	if((string)$params['my-date-period']===(string)"" or (string)$params['my-date-period']===(string)"сейчас"){
-		$temp=gg("ow_fact.temperature");
-		$humidity=gg("ow_fact.humidity");
-		$wind=gg("ow_fact.wind_speed");
-		$weather_type=gg("ow_fact.weather_type");
-		}
-	if((string)$params['my-date-period']===(string)"сегодня" or (string)$params['my-date-period']==="Сегодня"){
+	if($params['my-date-period']=="" or $params['my-date-period']=="сейчас" or $params['my-date-period']=="сегодня" or $params['my-date-period']=="Сегодня"){
 		$temp=gg("ow_fact.temperature");
 		$humidity=gg("ow_day0.humidity");
 		$wind=gg("ow_day0.wind_speed");
 		$weather_type=gg("ow_day0.weather_type");		
 		}
-    if((string)$params['my-date-period']===(string)"завтра"){
+    if((string)$params['my-date-period']=="завтра"){
 		$temp=gg("ow_day8.temperature");
 		$humidity=gg("ow_day8.humidity");
 		$wind=gg("ow_day8.wind_speed");
 		$weather_type=gg("ow_day8.weather_type");}
-    if((string)$params['my-date-period']==="послезавтра" or (string)$params['my-date-period']==="после завтра"){
+    if((string)$params['my-date-period']=="послезавтра" or (string)$params['my-date-period']=="после завтра"){
 		$temp=gg("ow_day16.temperature");
 		$humidity=gg("ow_day16.humidity");
 		$wind=gg("ow_day16.wind_speed");
 		$weather_type=gg("ow_day16.weather_type");
 		}
-$status.="Погода в ".gg("ow_city.name")." на данный момент: ".$weather_type.". "." Температура: ".round($temp)."  ".degree($temp)." цельсия. "." Относительная влажность: ".round($humidity)." ".humidity($humidity).". ".$stp.$stw;
-say($status,2);
+
+if($params['weather']=="погода"){
+	$status.="Погода в ".gg("ow_city.name")." на данный момент: ".$weather_type.". "." Температура: ".round($temp)."  ".degree($temp)." цельсия. "." Относительная влажность: ".round($humidity)." ".humidity($humidity).". ".$stp.$stw;
+	say($status,2);
 }
 
-	//если удалось определить время, то выдать результат
-if(!empty($temp) and !empty($humidity) and !empty($humidity)){
-	
-		if((string)$params['WeatherType']===(string)"тепло" and $temp>24){
+
+else if($params['WeatherType']=="тепло" and $temp>24){
         say("Да, ".$params['my-date-period']." на улице тепло. Можно идти в шортах и футболке. Не забудь взять солнечные очки.",2);
-        if((string)$weather_type===(string)"пасмурно" and $humidity>85) //влажность
+        if((string)$weather_type=="пасмурно" and $humidity>85) //влажность
       		say("И скорей всего будет дождь если уже не идёт.",2);
      	if($wind>10 and $wind<15)say(" И ещё меного дует ветер.",2);
         elseif($wind>16 and $wind<20)say(" И ещё поднялся сильный ветер.",2);
@@ -122,43 +116,43 @@ if(!empty($temp) and !empty($humidity) and !empty($humidity)){
     }
     
 
-	else if((string)$params['WeatherType']===(string)"тепло" and $temp>0 and $temp<25){
+else if((string)$params['WeatherType']=="тепло" and $temp>0 and $temp<25){
          say($params['my-date-period']." не очень тепло, на улице достаточно прохладно.",2);
-         if((string)$weather_type===(string)"пасмурно" and $humidity>85) //влажность
+         if((string)$weather_type=="пасмурно" and $humidity>85) //влажность
       		say("И скорей всего будет дождь если уже не идёт.",2);
      	if($wind>10 and $wind<15)say(" И ещё меного дует ветер.",2);
         elseif($wind>16 and $wind<20)say(" И ещё поднялся сильный ветер.",2);
         else if($wind>21)say(" Фигачит сильный ветер.",2);
     }
 
-	elseif((string)$params['WeatherType']===(string)"холодно" and $temp>25){
+else if((string)$params['WeatherType']=="холодно" and $temp>25){
      	say("Нет, ".$params['my-date-period']." на улице жара, не забудь взять солнечные очки.",2);
-     	if((string)$weather_type===(string)"пасмурно" and $humidity>85) //влажность
+     	if((string)$weather_type=="пасмурно" and $humidity>85) //влажность
       		say("Но скорей всего будет дождь если уже не идёт. Возьми зонтик.",2);
         if($wind>10 and $wind<15)say(" И ещё меного дует ветер.",2);
         else if($wind>16 and $wind<20)say(" И ещё поднялся сильный ветер.",2);
         else if($wind>21)say(" Фигачит сильный ветер.",2);
     }
 
-	elseif((string)$params['WeatherType']===(string)"холодно" and $temp>0 and $temp<25){
+else if((string)$params['WeatherType']=="холодно" and $temp>0 and $temp<25){
         say("Да, ".$params['my-date-period']." на улице достаточно прохладно и ".$weather_type.". Накинь что-нибуть сверху.",2);
-     	if((string)$weather_type===(string)"пасмурно" and $humidity>85) //влажность
+     	if((string)$weather_type=="пасмурно" and $humidity>85) //влажность
       		say("И скорей всего будет дождь если уже не идёт. Возьми зонтик.",2);
         if($wind>10 and $wind<15)say(" И ещё меного дует ветер.",2);
         else if($wind>16 and $wind<20)say(" И ещё поднялся сильный ветер.",2);
         else if($wind>21)say(" Фигачит сильный ветер.",2);
     }
 
-	elseif((string)$params['WeatherType']==(string)"холодно" and $temp>-10 and $temp<1){
+else if((string)$params['WeatherType']=="холодно" and $temp>-10 and $temp<1){
         say("Да, ".$params['my-date-period']." на улице небольшой мороз и ".$weather_type. ". Одевайся теплее.",2);
-        if((string)$weather_type==(string)"пасмурно" and $humidity>85) //влажность
+        if((string)$weather_type=="пасмурно" and $humidity>85) //влажность
       		say("И скорей всего будет снег если уже не идёт.",2);
         if($wind>10 and $wind<15)say(" И ещё меного дует ветер.",2);
         else if($wind>16 and $wind<20)say(" И ещё поднялся сильный ветер.",2);
         else if($wind>21)say(" Фигачит сильный ветер.",2);
     }
 
-	elseif((string)$params['WeatherType']===(string)"холодно" and $temp<-11){
+else if((string)$params['WeatherType']=="холодно" and $temp<-11){
         say("Да, ".$params['my-date-period']." на улице мороз и ".$weather_type." Тёплая шуба не помешает.",2);
         if($weather_type=="пасмурно" and $humidity>85) //влажность
       		say("И скорей всего будет снег если уже не идёт.",2);
@@ -167,33 +161,40 @@ if(!empty($temp) and !empty($humidity) and !empty($humidity)){
         else if($wind>21)say(" Фигачит сильный ветер.",2);
     }
 
-	if((string)$params['WeatherType']===(string)"ветрено"){
+else if((string)$params['WeatherType']=="ветрено"){
         if($wind<10)say("Нет, ".$params['my-date-period']." ветра почти нет.",2);
      	if($wind>=10 and $wind<15)say("Да, ".$params['my-date-period']." меного дует ветер.",2);
         else if($wind>16 and $wind<20)say("Да, ".$params['my-date-period']." поднялся сильный ветер.",2);
         else if($wind>21)say("Да, ".$params['my-date-period']." фигачит сильный ветер.",2);
     }
 
-	if((string)$params['WeatherType']===(string)"пасмурно"){
-        if((string)$weather_type===(string)"пасмурно")say("Да, ".$params['my-date-period']." на улице пасмурно.",2);
+else if((string)$params['WeatherType']=="пасмурно" or $params['WeatherType']=="облачно"){
+        if($weather_type=="пасмурно" or $weather_type=="облачно")say("Да, ".$params['my-date-period']." на улице ".$weather_type,2);
         else say("Нет, на улице ".$weather_type,2);     
     }
 
-	if((string)$params['WeatherType']===(string)"ясно"){
-        if((string)$weather_type===(string)"ясно")say("Да, ".$params['my-date-period']." на улице ясно.",2);
+else if((string)$params['WeatherType']=="ясно"){
+        if((string)$weather_type=="ясно")say("Да, ".$params['my-date-period']." на улице ясно.",2);
         say("Нет, на улице ".$weather_type,2);
     }
 
-	if((string)$params['WeatherType']===(string)"влажно")say($params['my-date-period']." влажность на улице ".$humidity." ".humidity($humidity),2);
+	if((string)$params['WeatherType']=="влажно")say($params['my-date-period']." влажность на улице ".$humidity." ".humidity($humidity),2);
 
 	
-	if((string)$params['WeatherType']===(string)"гроза" or (string)$params['WeatherType']===(string)"дождь" or (string)$params['WeatherType']===(string)"осадки" or (string)$params['WeatherType']===(string)"легкий дождь"){
-     	if($weather_type===(string)"гроза" or $weather_type===(string)"дождь" or $weather_type===(string)"легкий дождь")
+else if($params['WeatherType']=="гроза" or $params['WeatherType']=="дождь" or $params['WeatherType']=="осадки" or $params['WeatherType']=="легкий дождь" or $params['WeatherType']=="ливень"){
+     	if($weather_type=="гроза" or $weather_type===(string)"дождь" or $weather_type=="легкий дождь" or $weather_type=="ливень")
      	say("Да, на улице ".$params['my-date-period']." ".$weather_type,2);
-     	elseif($weather_type===(string)"пасмурно")say("На улице пасмурно, возможно пойдёт дождь.",2);
      	else say("Нет, на улице не ожидаются осадки. ".$params['my-date-period']." будет ".$weather_type,2);
     }
-     
-}//конец глобальной проверки на определение времени
+else if((string)$params['WeatherType']=="снег"){
+        if($weather_type=="небольшой снегопад" or $weather_type=="снег" or $weather_type=="снегопад")say("Да, ".$params['my-date-period']." на улице ".$weather_type,2);
+        else say("Нет, на улице ".$weather_type,2); 
+}
+
+
+
+
+
+
 	
 
